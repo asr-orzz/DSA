870b12ef84d9535ad8081ef09031d896943684d6
asr-orzz <aneeshsinghrajoriya@gmai.com>
2026-08-25 20:02:20 +0530

    template

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+ 
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+  
+    int _t;
+    cin >> _t;
+    while (_t--) {
+       
+    }
+ 
+    return 0;
+}
